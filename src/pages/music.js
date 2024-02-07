eeaeeaa434b7b2d90d155240a1653fb9d668cb03
dreamlabs24 <mccuemedialabs@gmail.com>
2024-02-07 16:46:25 -0500

    mini navbar

---
+++ src/pages/music.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const Music = () => {
+  return (
+    <Layout pageTitle="music">
+      <p>jam jam jam</p>
+    </Layout>
+  )
+}
+
+export const Head = () => <title>music</title>
+
+export default Music
